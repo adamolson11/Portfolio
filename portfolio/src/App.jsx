3fadb4f8ed98a50a0685a4919179f607ddfbcf54
Adam Olson <adam.lee.olson@outlook.com>
2024-02-28 13:20:56 -0600

    first commit, up and running

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,18 @@
+
+
+
+const App = () => {
+
+  // const [count, setCount] = useState(0)
+
+  return <div> 
+    <section>Hero</section>
+    <section>Parallax</section>
+    <section>Services</section>
+    <section>Portfolio1</section>
+    <section>Portfolio2</section>
+    <section>Portfolio3</section>
+    </div>
+};
+
+export default App
