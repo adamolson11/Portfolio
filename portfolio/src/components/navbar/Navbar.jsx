56f7780163fe2971c30fd921d7e6596f2b4563af
Adam Olson <adam.lee.olson@outlook.com>
2024-02-28 14:52:57 -0600

    testing sidebar 16:32

--- portfolio/src/components/navbar/Navbar.jsx
+++ portfolio/src/components/navbar/Navbar.jsx
@@ -9,9 +9,9 @@ const Navbar = () => {
             <span> Adam Olson</span>
         <div className= "social">
             <a href = "#"><img src = "facebook.png" alt = "" /> </a>
-            <a href = "#"><img src = "linkedIn.png" alt = "" /> </a>
-            <a href = "#"><img src = "GitHub.png" alt = "" /> </a>
-            <a href = "#"><img src = "YouTube.png" alt = "" /> </a>
+            <a href = "#"><img src = "facebook.png" alt = "" /> </a>
+            <a href = "#"><img src = "instagram.png" alt = "" /> </a>
+            <a href = "#"><img src = "youtube.png" alt = "" /> </a>
         </div>
     </div>
     </div>
