d75d4e2202b25b2440d7ca8cb1c520480552b71b
Adam Olson <adam.lee.olson@outlook.com>
2024-03-16 10:06:09 -0500

    creating hero section

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -2,12 +2,14 @@
 
 // import Test from "./components/Test";
 import "./app.scss";
+import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar"; 
 import Sidebar from "./components/sidebar/Sidebar"; // Correct import path and uppercase component name
 const App = () => {
   return <div>
     <section id = "Homepage">
       <Navbar/>
+      <Hero/>
       </section>
       <section id = "Services">Parallax</section>
       <section>Services</section>
--- portfolio/src/components/sidebar/links/Links.jsx
+++ portfolio/src/components/sidebar/links/Links.jsx
@@ -1,22 +1,49 @@
+import { motion } from "framer-motion";
+
+const variants = {
+  open: {
+    transition: {
+      staggerChildren: 0.1, // Corrected property name to staggerChildren
+    }, 
+  }, 
+  closed: {
+    transition: {
+      staggerChildren: 0.05,
+      staggerDirection: -1, // Corrected property name to staggerDirection
+    },
+  },
+};
+
+const itemVariants = {
+  open: {
+    y: 0, 
+    opacity: 1,
+  },
+  closed: {
+    y: 50, 
+    opacity: 0, 
+  },
+};
+
 /* eslint-disable no-unused-vars */
 const Links = () => {
-   const items = [
-     "Homepage",
-     "Services",
-     "Portfolio",
-     "Contact",
-     "About"
-   ];
-   
-   return (
-     <div className="links">
-       {items.map((item, index) => (
-         <a key={index} href={`#${item}`}>
-            {item}
-         </a>
-       ))}
-     </div>
-   );
-}
+  const items = [
+    "Homepage",
+    "Services",
+    "Portfolio",
+    "Contact",
+    "About"
+  ];
+
+  return (
+    <motion.div className="links" variants={variants}>
+      {items.map((item, index) => (
+        <motion.a key={index} href={`#${item}`} variants={itemVariants} whileHover= {{scale:1.1}} whileTape ={{scale: 0.95}}>
+          {item}
+        </motion.a>
+      ))}
+    </motion.div>
+  );
+};
 
 export default Links;
