b7524b8ca0f5f6aadbc0ba7b50c63b6ee3b96bf5
Adam Olson <adam.lee.olson@outlook.com>
2024-03-16 11:33:48 -0500

    finishing hero section

--- portfolio/src/components/hero/Hero.jsx
+++ portfolio/src/components/hero/Hero.jsx
@@ -1,12 +1,46 @@
 import "./hero.scss" 
+import {motion} from "framer-motion"; 
+const textVariants = {
+    initial: {
+        x: -500, 
+        opacity: 0,
+    },
+
+    animate: {
+        x: 0, 
+        opacity: 1,
+        transition: {
+            duration: 1, 
+            staggerChildren: 0.1
+        }
+    },
+}
 
 const Hero = () => {
     return (
         <div className="hero"> 
+        <div className = "wrapper">
+        <motion.div 
+        className= "textContainer"
+         variants= {textVariants}
+          initial= "initial" 
+          animate= "animate"
+          >
+            <motion.h2 variants= {textVariants}>ADAM OLSON</motion.h2>
+            <motion.h1 variants= {textVariants}> Full Stack Web Development </motion.h1>
+            <motion.div variants= {textVariants} className= "buttons">
+                <motion.button variants= {textVariants}> Check out my work </motion.button>
+                <motion.button variants= {textVariants}> Contact Me </motion.button>
+                </motion.div>
+                <motion.img variants= {textVariants} src = "/scroll.png" alt= "" />
+            </motion.div>
+        </div>
+        <div className = "slidingTextContainer"> Writer. Developer. Marketer. Dad. </div>
         <div className= "imageContainer"> 
             <img src= "/hero.png" alt= "" />
         </div>
         </div>
+        
         )
 }
 
