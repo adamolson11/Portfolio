f392085cad562c9eac0d6b2794b3e8619a2f54a8
Adam Olson <adam.lee.olson@outlook.com>
2024-03-19 14:08:48 -0500

    still working on services, about half way done

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -6,6 +6,8 @@ import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar"; 
 import Sidebar from "./components/sidebar/Sidebar"; // Correct import path and uppercase component name
 import Parallax from "./components/parallax/Parallax"; 
+import Services from "./components/services/Services"; 
+
 const App = () => {
   return <div>
     <section id = "Homepage">
@@ -13,7 +15,7 @@ const App = () => {
       <Hero/>
       </section>
       <section id = "Services"><Parallax type = "services" /> </section>
-      <section>Services</section>
+      <section><Services/></section>
       <section id ="Portfolio"><Parallax type="portfolio"/></section>
       <section>Portfolio1</section>
       <section>Portfolio2</section>
---
+++ portfolio/src/components/services/Services.jsx
@@ -0,0 +1,92 @@
+/* eslint-disable no-unused-vars */
+import "./services.scss";
+import { motion } from "framer-motion";
+
+
+const variants= {
+    initial:{
+        x:-500, 
+        y: 100,
+        opacity:0 
+
+    },
+    animate: {
+        x:0, 
+        opacity: 1, 
+        y: 0, 
+        transition: {
+            duration: 1, 
+            staggerChildren: 0.1, 
+        }
+    }
+}
+const Services = () => {
+    return (
+        <motion.div 
+        className="services" 
+        variants= {variants}
+         initial = "initial"
+          animate= "animate">
+            Services
+            <motion.div className="textContainer"  variants= {variants}>
+                <p>Together.<br/> we move forward.</p>
+                <hr/>
+            </motion.div>
+
+            <motion.div className="titleContainer"  variants= {variants}>
+                <div className="title">
+                    <img src="/people.png" alt="image of people working in an office" />
+                    <h1>
+                        New Ideas 
+                        <b> For Your </b> 
+                        Business.
+                    </h1>
+                    <button> WHAT I DO. </button>
+                </div>
+            </motion.div>
+
+            <motion.div className="listContainer"  variants= {variants}>
+                <motion.div className="card">
+                    <motion.div className="box" whileHover={{ background: "lightgray", color: "black" }}>
+                        <h2>Branding</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, culpa enim! 
+                        Quae labore aliquam, suscipit praesentium recusandae facere ducimus quidem r
+                        eprehenderit tempora! Cumque dolore ab nesciunt modi nam impedit iste?</p>
+                        <button>GO</button>
+                    </motion.div>
+                </motion.div>
+
+                <motion.div className="card">
+                    <motion.div className="box" whileHover={{ background: "lightgray", color: "black" }}>
+                        <h2>Branding</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, culpa enim! 
+                        Quae labore aliquam, suscipit praesentium recusandae facere ducimus quidem r
+                        eprehenderit tempora! Cumque dolore ab nesciunt modi nam impedit iste?</p>
+                        <button>GO</button>
+                    </motion.div>
+                </motion.div>
+
+                <motion.div className="card">
+                    <motion.div className="box" whileHover={{ background: "lightgray", color: "black" }}>
+                        <h2>Branding</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, culpa enim! 
+                        Quae labore aliquam, suscipit praesentium recusandae facere ducimus quidem r
+                        eprehenderit tempora! Cumque dolore ab nesciunt modi nam impedit iste?</p>
+                        <button>GO</button>
+                    </motion.div>
+                </motion.div>
+                <motion.div className="card">
+                    <motion.div className="box" whileHover={{ background: "lightgray", color: "black" }}>
+                        <h2>Branding</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, culpa enim! 
+                        Quae labore aliquam, suscipit praesentium recusandae facere ducimus quidem r
+                        eprehenderit tempora! Cumque dolore ab nesciunt modi nam impedit iste?</p>
+                        <button>GO</button>
+                    </motion.div>
+                </motion.div>
+            </motion.div>
+        </motion.div>
+    );
+};
+
+export default Services;
